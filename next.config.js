35a3c9ef299c8c52656520c9d616f8bde175088a
dan-w1231 <dannywallace1231@gmail.com>
2024-05-20 09:33:27 +0100

    change config to export static site for github pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+}
 
 // module.exports = nextConfig
 module.exports = {
+  nextConfig,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+
 }
