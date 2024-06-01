35e3f312eeaec45033240660dd6dcb341622c7db
dan-w1231 <dannywallace1231@gmail.com>
2024-06-01 04:33:28 +0100

    add back temporary image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,8 +53,81 @@ export function Hero() {
             </div>
             <div className="absolute top-0 right-0 w-full h-full z-[2]">
               {/* // header image stuff */}
-              
-              </div>
+                            
+              <motion.div
+
+                className="absolute w-full h-full right-0 top-0"
+                initial={{ opacity: 0, x: 60, rotate: 0 }}
+                whileInView={{ opacity: 1, x: 0, rotate: 0 }}
+                transition={{ type: "spring", stiffness: 100, delay: 1, duration: 6 }}
+              >
+                  <motion.div
+                    animate={{ x: -20 }}
+                    transition={{ duration: 8, repeat: Infinity, delay: 2, repeatType: "mirror", transition:{ type: "spring"} }}
+                  >
+                    <Image
+                      id="reflection"
+                      src={marbleSphere}
+                      alt="" className="absolute 
+                        w-[10rem] sm:w-[14rem] md:w-[54rem] 
+                        top-[6px] sm:top-[13px] xl:top-[-238px]
+                        right-[-71px] md:right-[-230px] xl:right-[-245px] z-[-97] md:rotate-[30deg] 
+                        blur-[32px] md:blur-[130px] 
+                        opacity-75 lg:opacity-80 rotate-[-60deg] md:rotate(0deg)"
+                      unoptimized
+                    />
+                  </motion.div>
+                  <motion.div
+                    animate={{ rotate: 0, x: -6 }}
+                    transition={{ duration: 8, repeat: Infinity, repeatType: "mirror", transition:{ type: "spring"} }}
+                  >
+                  <Image
+                    src={marbleSphere}
+                    alt=""
+                    className="absolute 
+                      w-[14rem] md:w-[35rem] xl:w-[54rem] 
+                      top-[-80px] sm:top-[13px] md:top-[-72px] xl:top-[-238px]
+                      right-[-112px] md:right-[-230px] xl:right-[-245px]
+                      z-[-97] 
+                      md:rotate-[12deg]"
+                    unoptimized
+                  />
+                  </motion.div>
+                  <motion.div
+                    animate={{ rotate: 0, x: -5, z: 0 }}
+                    transition={{ duration: 8, repeat: Infinity, delay: 0.1, repeatType: "mirror", transition:{ type: "spring"} }}
+                  >
+                  <Image
+                    src={mL2}
+                    alt=""
+                    className="absolute 
+                      w-[14rem] md:w-[35rem] xl:w-[54rem] 
+                      top-[-80px] sm:top-[13px] md:top-[-72px] xl:top-[-238px]
+                      right-[-112px] md:right-[-230px] xl:right-[-245px]
+                      z-[-95] 
+                      md:rotate-[12deg]"
+                    unoptimized
+                  />
+                  </motion.div>
+                  <motion.div
+                    animate={{rotate: 0, x: -7, z: -1}}
+                    transition={{ duration: 8, repeat: Infinity, delay: 2, repeatType: "mirror", transition:{ type: "spring"} }}
+                  >
+                  <Image
+                    src={mL3}
+                    alt=""
+                    className="absolute 
+                      w-[14rem] md:w-[35rem] xl:w-[54rem] 
+                      top-[-80px] sm:top-[13px] md:top-[-72px] xl:top-[-238px]
+                      right-[-112px] md:right-[-230px] xl:right-[-245px]
+                      z-[-94] 
+                      md:rotate-[12deg]"
+                    unoptimized
+                  />
+                  </motion.div>
+
+              </motion.div>
+            </div>
               {/* <Image src={nightBg} alt="" className="absolute invisible md:visible right-[-100px] xl:right-0 xl:rotate[5deg] top-[-280px] xl:w-[898px] xl:rotate-[14deg]" unoptimized/> */}
               {/* <motion.div
                 className="absolute w-full right-[-200px] top-[-400px]"
