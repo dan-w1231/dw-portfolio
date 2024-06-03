2ccbaa32b694c43590f3ab4de4fd67991b77b97f
dan-w1231 <dannywallace1231@gmail.com>
2024-06-03 19:58:35 +0100

    vercel.json

--- src/pages/api/SendEmail.ts
+++ src/pages/api/SendEmail.ts
@@ -2,14 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { sendEmail } from "../../lib/sendgrid";
 
 export default async function sendEmailRoute(req: NextApiRequest, res: NextApiResponse) {
-
-  res.setHeader('Access-Control-Allow-Origin', 'https://dwdesign.io');
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.dwdesign.io');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
   if (req.method === 'OPTIONS') {
-    // Pre-flight request stuff?
     res.status(200).end();
     return;
   }
