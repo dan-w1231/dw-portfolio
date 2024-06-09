dcb7be53d9df3e4dfc93a7071c212da5d0ef9985
dan-w1231 <dannywallace1231@gmail.com>
2024-06-09 19:17:21 +0100

    remove useclient on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { type Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import clsx from 'clsx';
