f57de6448fb5991095657c6b5d2d5e78749d08f1
dan-w1231 <dannywallace1231@gmail.com>
2024-06-10 15:01:22 +0100

    remove export from nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
   basePath: "/dw-portfolio",
   reactStrictMode: true,
 }
@@ -26,10 +25,7 @@ module.exports = {
 
     return config;
   },
-  nextConfig,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
